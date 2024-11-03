4e66edb56281355a636f1a7f3351d488f28acfe7
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-03 10:10:32 -0300

    feat: add cache entry at the loggout process

--- apps/logout/src/logout.module.ts
+++ apps/logout/src/logout.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { LogoutController } from './logout.controller';
 import { LogoutService } from './logout.service';
+import { CacheLibraryModule } from '@libs/cache-library/cache-library.module';
 
 @Module({
   imports: [
     JwtModule.register({
       global: true,
     }),
+    CacheLibraryModule,
   ],
   controllers: [LogoutController],
   providers: [LogoutService],
--- apps/logout/src/logout.service.ts
+++ apps/logout/src/logout.service.ts
@@ -1,12 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { LogoutSerializer } from './serializers/logout.serializer';
+import { CacheKeyPrefix } from '@libs/cache-library/enums/cache-key-prefix.enum';
 
 @Injectable()
 export class LogoutService {
+  constructor(@Inject(CACHE_MANAGER) private cacheService: Cache) {}
+
   async logout(userId: string, loginId: string): Promise<LogoutSerializer> {
+    const performedAt = new Date().getTime();
+
+    await this.cacheService.set(
+      [CacheKeyPrefix.AUTH_SESSION_LOGOUT, userId, loginId].join(':'),
+      {
+        performedAt,
+      },
+      {
+        // the refreshToken is the one with the greater expire time
+        ttl: this.convertStringToSeconds(process.env.JWT_REFRESH_EXPIRES_IN),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any,
+    );
+
     return {
       performed: true,
-      performedAt: new Date().getTime(),
+      performedAt,
     };
+  }
+
+  /**
+   * Convertes a string to seconds
+   *
+   * @param timeString Time to be converted. Ex: 3m, 1h, 2d
+   * @returns Time converted to seconds
+   */
+  convertStringToSeconds(timeString: string): number {
+    const [value, type] = timeString.split(/(\d+)/).filter(Boolean);
+
+    let multiply = 1;
+
+    switch (type) {
+      case 'd':
+        multiply = 86400; // 24 * 60 * 60;
+        break;
+      case 'h':
+        multiply = 3600; // 60 * 60;
+        break;
+      case 'm':
+        multiply = 60;
+        break;
+    }
+
+    return parseInt(value) * multiply;
   }
 }
