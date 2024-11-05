d564acd879d6d16e7e21aa40c7f701f1cedd1dc0
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-05 10:41:39 -0300

    refactor: clean up unused code

--- libs/database-library/src/repositories/user-auths/user-auths.repository.ts
+++ libs/database-library/src/repositories/user-auths/user-auths.repository.ts
@@ -11,31 +11,6 @@ export class UserAuthsRepository {
   ) {}
 
   /**
-   * Verifies if the given userId OR username already exists.
-   * If they exists at different entities, both entities will be returned
-   *
-   * @param userId ID to be retrieved
-   * @param username Username to be retrieved
-   * @returns List with the found entities. This can have zero, one or two items
-   */
-  async findByIdOrUsername(
-    userId: string,
-    username: string,
-  ): Promise<UserAuthEntity[]> {
-    return await this.repository.find({
-      where: [{ userId }, { username }],
-    });
-  }
-
-  async findById(userId: string): Promise<UserAuthEntity> {
-    return await this.repository.findOne({
-      where: {
-        userId,
-      },
-    });
-  }
-
-  /**
    * Finds a entity (user) by its username.
    *
    * @param username Username to be found.
@@ -47,13 +22,5 @@ export class UserAuthsRepository {
         username,
       },
     });
-  }
-
-  async insert(entity: UserAuthEntity): Promise<InsertResult> {
-    return await this.repository.insert(entity);
-  }
-
-  async save(entity: UserAuthEntity): Promise<UserAuthEntity> {
-    return await this.repository.save(entity);
   }
 }
