34b3d17d23f21accc2fb3f80f45c7d781184eb95
shimisnow <14943507+shimisnow@users.noreply.github.com>
2024-11-08 10:25:47 -0300

    docs: add swagger decorator for logout app

--- apps/logout/src/logout.controller.ts
+++ apps/logout/src/logout.controller.ts
@@ -1,17 +1,50 @@
-import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, Post, UseGuards, Version } from '@nestjs/common';
 import { LogoutService } from './logout.service';
 import { UserPayload } from '@libs/auth/payloads/user.payload';
 import { User } from '@libs/auth/decorators/user.decorator';
 import { AuthGuard } from '@libs/auth/guards/auth.guard';
 import { LogoutSerializer } from './serializers/logout.serializer';
+import {
+  ApiBadGatewayResponse,
+  ApiBearerAuth,
+  ApiHeader,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { DefaultError500Serializer } from './serializers/default-error-500.serializer';
+import { DefaultError502Serializer } from './serializers/default-error-502.serializer';
 
 @Controller('logout')
 export class LogoutController {
   constructor(private readonly logoutService: LogoutService) {}
 
+  @Version('1')
   @Post()
   @HttpCode(200)
   @UseGuards(AuthGuard)
+  @ApiBearerAuth('AccessToken')
+  @ApiHeader({
+    name: 'X-Api-Version',
+    description: 'Sets the API version',
+    required: true,
+  })
+  @ApiOperation({
+    summary: 'Invalidates all tokens issued for a give login request',
+  })
+  @ApiOkResponse({
+    description: 'Information if the logout process was performed',
+    type: LogoutSerializer,
+  })
+  @ApiInternalServerErrorResponse({
+    description:
+      'The server has encountered a situation it does not know how to handle. See server logs for details',
+    type: DefaultError500Serializer,
+  })
+  @ApiBadGatewayResponse({
+    description: 'Internal data processing error. Probably a database error',
+    type: DefaultError502Serializer,
+  })
   async logout(@User() user: UserPayload): Promise<LogoutSerializer> {
     return await this.logoutService.logout(user.userId, user.loginId);
   }
--- apps/logout/src/main.ts
+++ apps/logout/src/main.ts
@@ -1,15 +1,42 @@
 import { NestFactory } from '@nestjs/core';
-import { LogoutModule } from './logout.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { mkdirSync, writeFileSync } from 'fs';
+import { LogoutModule } from './logout.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(LogoutModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Logout Function')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        description: 'accessToken provided by the login endpoint',
+        in: 'header',
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'AccessToken',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  await mkdirSync('apps/logout/docs/openapi/', { recursive: true });
+  await writeFileSync(
+    'apps/logout/docs/openapi/openapi-docs.json',
+    JSON.stringify(document),
+    { encoding: 'utf8' },
+  );
+
   const port = process.env.SERVICE_LOGOUT_PORT ?? 3000;
   await app.listen(port);
   console.log(`Application running at port ${port}`);
